8bb55a72469eeef4365abf767c7484838b03258b
logrcia <lolagm04@gmail.com>
2024-10-24 09:22:26 -0300

    seleccion de dados

--- src/js/js.generala.js
+++ src/js/js.generala.js
@@ -10,27 +10,48 @@ function rollDice() {
 let dados;
 let dadosSeleccionados;
 
-    const initGame = () => {
-        dados = [0, 0, 0, 0, 0];
-        dadosSeleccionados = [false, false, false, false, false];
-    };
+const initGame = () => {
+    dados = ["", "", "", "", ""]; 
+    dadosSeleccionados = [false, false, false, false, false];
 
-    const drawDices = () => {
-        dados.forEach((dado, i) => {
-            document.getElementById(`dado-${i + 1}`).innerHTML = dado; 
-        });
-    };
+    document.querySelectorAll(".dados-container .dados").forEach((diceElement, i) => {
+        diceElement.addEventListener("click", () => toggleDiceSelection(i));
+    });
 
-    const rollDices = () => {
-        for (let i = 0; i < dados.length; i++) {
-            if(dadosSeleccionados){
-                dados[i] = Math.floor(Math.random() * 6) + 1;
-            }
+    drawDices();
+};
+
+const drawDices = () => {
+    dados.forEach((dado, i) => {
+        const diceElement = document.querySelector(`.dados-container .dado-${i + 1}`);
+        if (diceElement) {
+            diceElement.innerHTML = dado;  // Muestra el valor del dado
         }
-        dadosSeleccionados = [false, false, false, false, false];
-        drawDices();
-    };
+    });
+};
 
-    document.getElementById("roll-btn").addEventListener("click", rollDices);
+const rollDices = () => {
+    for (let i = 0; i < dados.length; i++) {
+        if (!dadosSeleccionados[i]) {  // Solo lanza los dados que no están seleccionados
+            dados[i] = Math.floor(Math.random() * 6) + 1;  // Genera un número entre 1 y 6
+        }
+    }
+    drawDices();  // Después de tirar los dados, actualiza la interfaz
+};
 
-    document.addEventListener("DOMContentLoaded", () => { initGame(); });
+const toggleDiceSelection = diceNumber => {
+    dadosSeleccionados[diceNumber] = !dadosSeleccionados[diceNumber];
+    const diceElement = document.querySelector(`.dados-container .dado-${diceNumber + 1}`);
+    if (dadosSeleccionados[diceNumber]) {
+        diceElement.classList.add("dadosSeleccionados");
+    } else {
+        diceElement.classList.remove("dadosSeleccionados");
+    }
+    console.info("Dices selection " + dadosSeleccionados);
+};
+
+document.getElementById("roll-btn").addEventListener("click", rollDices);
+
+document.addEventListener("DOMContentLoaded", () => {
+    initGame();
+});
